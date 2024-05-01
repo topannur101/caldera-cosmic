44611a22ed50b948c356362286e42495cadbab7c
Andi Permana <andi.thea96@gmail.com>
2024-05-01 23:32:42 +0700

    Chart update

---
+++ resources/views/livewire/insight/rtc/manage/recipes-form.blade.php
@@ -0,0 +1,82 @@
+<?php
+
+use Livewire\Volt\Component;
+use App\Models\InsRtcRecipe;
+use Livewire\Attributes\Renderless;
+
+new class extends Component {
+    public InsRtcRecipe $recipe;
+    public $recipe_id = '';
+
+    public $is_superuser = false;
+
+    public function rules()
+    {
+        return [
+            'name'      => ['required','min:1', 'max:128'],
+            'og_rs'     => ['required','min:1', 'max:4'],
+            'std_min'   => ['required', 'numeric', 'min:0', 'max:10'],
+            'std_max'   => ['required', 'numeric', 'min:0', 'max:10'],
+            'std_mid'   => ['required', 'numeric', 'min:0', 'max:10'],
+        ];
+    }
+
+    // public function messages()
+    // {
+    //     return [
+    //         'user_id.exists' => __('Tag hanya boleh berisi huruf, angka, dan strip')
+    //     ];
+    // }
+
+    public function placeholder()
+    {
+        return view('livewire.layout.modal-placeholder');
+    }
+
+    public function mount(InsRtcRecipe $recipe)
+    {
+        if ($recipe->id) {
+            $this->recipe_id = $recipe->id;
+            // other properties
+        }
+        $this->is_superuser = Gate::allows('superuser');
+    }
+
+    public function save()
+    {
+        Gate::authorize('superuser');
+
+        // VALIDATE
+
+        $this->validate();
+
+        $this->js('window.dispatchEvent(escKey)');
+    }
+
+    public function delete()
+    {
+        $this->recipe->delete();
+        $this->js('window.dispatchEvent(escKey)');
+        $this->js('notyf.success("' . __('Resep dihapus') . '")');
+        $this->dispatch('updated');
+    }
+
+};
+
+?>
+
+<div>
+    <form wire:submit="save" class="p-6">
+        <div class="flex justify-between items-start">
+            <h2 class="text-lg font-medium text-neutral-900 dark:text-neutral-100">
+                {{ __('Resep') }}
+            </h2>
+            <x-text-button type="button" x-on:click="$dispatch('close')"><i class="fa fa-times"></i></x-text-button>
+        </div>
+      
+    </form>
+    <x-spinner-bg wire:loading.class.remove="hidden" wire:target="delete"></x-spinner-bg>
+    <x-spinner wire:loading.class.remove="hidden" wire:target="delete" class="hidden"></x-spinner>
+    <x-spinner-bg wire:loading.class.remove="hidden" wire:target="save"></x-spinner-bg>
+    <x-spinner wire:loading.class.remove="hidden" wire:target="save" class="hidden"></x-spinner>
+</div>
--- resources/views/livewire/insight/ss/3.blade.php
+++ resources/views/livewire/insight/ss/3.blade.php
@@ -11,102 +11,136 @@ new class extends Component {}; ?>
 
 @script
     <script>
-    let optionsLeft = {
-        chart: {
-            type: 'line',
-            height: 350,
-            animations: {
-                enabled: true,
-                easing: 'linear',
-                dynamicAnimation: {
-                    speed: 1000
+        let oLeftPts = {
+            chart: {
+                height: 350,
+                animations: {
+                    enabled: false,
+                    animateGradually: {
+                        enabled: false,
+                    },
+                    dynamicAnimation: {
+                        enabled: false,
+                    }
                 }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            series: [{
+                name: 'Left',
+                type: 'area',
+                data: []
+            },{
+                name: 'Middle',
+                type: 'line',
+                data: []
+            }],
+            xaxis: {
+                type: 'datetime',
+                range: 60000
+            },
+            yaxis: {
+                min: 0,
+                max: 10
+
+            },
+            stroke: {
+                curve: 'smooth'
             }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        series: [{
-            data: []
-        }],
-        xaxis: {
-            type: 'datetime',
-            range: 60000
-        },
-        yaxis: {
-            min: 0,
-            max: 10
+        };
 
-        },
-        stroke: {
-            curve: 'smooth'
-        }
-    };
-
-    let optionsRight = {
-        chart: {
-            type: 'line',
-            height: 350,
-            animations: {
-                enabled: true,
-                easing: 'linear',
-                dynamicAnimation: {
-                    speed: 1000
+        let oRightPts = {
+            chart: {
+                type: 'line',
+                height: 350,
+                animations: {
+                    enabled: true,
+                    easing: 'linear',
+                    dynamicAnimation: {
+                        speed: 1000
+                    }
                 }
+            },
+            dataLabels: {
+                enabled: false
+            },
+            series: [{
+                data: []
+            }],
+            xaxis: {
+                type: 'datetime',
+                range: 60000
+            },
+            yaxis: {
+                min: 0,
+                max: 10
+
+            },
+            stroke: {
+                curve: 'smooth'
             }
-        },
-        dataLabels: {
-            enabled: false
-        },
-        series: [{
-            data: []
-        }],
-        xaxis: {
-            type: 'datetime',
-            range: 60000
-        },
-        yaxis: {
-            min: 0,
-            max: 10
+        };
 
-        },
-        stroke: {
-            curve: 'smooth'
-        }
-    };
+        let leftChart = new ApexCharts(document.querySelector("#chart-left"), oLeftPts);
+        let rightChart = new ApexCharts(document.querySelector("#chart-right"), oRightPts);
 
-    let leftChart = new ApexCharts(document.querySelector("#chart-left"), optionsLeft);
-    let rightChart = new ApexCharts(document.querySelector("#chart-right"), optionsRight);
+        leftChart.render();
+        rightChart.render();
 
-    leftChart.render();
-    rightChart.render();
+        const leftPts   = oLeftPts.series[0].data;
+        const leftMids  = oLeftPts.series[1].data;
+        const rightPts  = oRightPts.series[0].data;
 
-    const leftPoints    = optionsLeft.series[0].data;
-    const rightPoints   = optionsRight.series[0].data;
+        setInterval(function() {
+            // Generate or fetch new data point
+            // axios.get('{{ route('insight.rtc.latest', ['device_id' => 1]) }}')
+            //     .then(response => {
+            //         let x = new Date(response.data.data.dt_client).getTime();
+            //         let y = 0;
 
-    setInterval(function () {
-        // Generate or fetch new data point
-        axios.get('{{ route('insight.rtc.latest', ['device_id' => 1]) }}')
-        .then(response => {
-            let x   = new Date(response.data.data.dt_client).getTime();
-            let y = 0;
+            //         // y   = parseFloat(response.data.data.act_left);
+            //         leftPts.push({
+            //             x,
+            //             y
+            //         });
+            //         leftChart.updateSeries([{
+            //             data: leftPts
+            //         }]);
 
-            y   = parseFloat(response.data.data.act_left);
-            leftPoints.push({x, y});
+            //         y = parseFloat(response.data.data.act_right);
+            //         rightPts.push({
+            //             x,
+            //             y
+            //         });
+            //         rightChart.updateSeries([{
+            //             data: rightPts
+            //         }]);
+            //     })
+            //     .catch(error => {
+            //         console.error('Error fetching data:', error);
+            //     });
+            // Run simulation
+            let x   = new Date().getTime();
+            let y = 3.5;
+
+            // y   = parseFloat(response.data.data.act_left);
+            leftPts.push({x, y});
+            y = 2.0;
+            leftMids.push({x, y});
             leftChart.updateSeries([{
-                data: leftPoints
+                name: 'Left',
+                data: leftPts
+            },{
+                name: 'Middle',
+                data: leftMids
             }]);
 
-            y = parseFloat(response.data.data.act_right);
-            rightPoints.push({x, y});
+            rightPts.push({x, y});
             rightChart.updateSeries([{
-                data: rightPoints
+                data: rightPts
             }]);
-        })
-        .catch(error => {
-            console.error('Error fetching data:', error);
-        });
 
-    }, 1000);
+        }, 1000);
     </script>
 @endscript
