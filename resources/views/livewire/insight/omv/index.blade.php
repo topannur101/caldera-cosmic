5776d797dcc8e01fda62369fa1bf86a80afdb06d
Andi Permana/Senior Staff/TT Manufacturing Modernization <andi.permana@taekwang.com>
2024-07-12 16:35:16 +0700

    OMV updates

--- resources/views/livewire/insight/omv/index.blade.php
+++ resources/views/livewire/insight/omv/index.blade.php
@@ -15,33 +15,22 @@ class extends Component {};
 </x-slot>
 
 <div id="content" class="py-6 max-w-7xl mx-auto sm:px-6 lg:px-8 text-neutral-800 dark:text-neutral-200">
-   <div class="grid grid-cols-4 gap-x-3">
-      <div class="bg-white dark:bg-neutral-800 shadow rounded-lg">
-         Hey
+   <div class="bg-white dark:bg-neutral-800 shadow rounded-lg mb-3 px-6 py-4">Batch information here</div>
+   <div class="grid grid-cols-4 gap-x-3 h-96">
+      <div class="bg-white dark:bg-neutral-800 shadow rounded-lg p-4">
+         <div class="mb-3">Step 1</div>
+         <div class="w-full bg-gray-200 rounded-full h-1.5 mb-4 dark:bg-gray-700">
+         <div class="bg-caldy-600 h-1.5 rounded-full dark:bg-caldy-500" style="width: 45%"></div>
+         </div>
       </div>
-      <div class="bg-white dark:bg-neutral-800 shadow rounded-lg">
-         Hey
+      <div class="bg-white dark:bg-neutral-800 shadow rounded-lg p-4">
+         Step 2
       </div>
-      <div class="bg-white dark:bg-neutral-800 shadow rounded-lg">
-         Hey
+      <div class="bg-white dark:bg-neutral-800 shadow rounded-lg p-4">
+         Step 3
       </div>
-      <div class="bg-white dark:bg-neutral-800 shadow rounded-lg">
-         Hey
+      <div class="bg-white dark:bg-neutral-800 shadow rounded-lg p-4">
+         Step 4
       </div>
    </div>
-    {{-- <div class="flex flex-col gap-x-2 sm:flex-row">
-        <div class="p-1">
-            <livewire:insight.omv.index-hides />
-        </div>
-        <div class="w-full overflow-hidden p-1">
-            <div class="relative bg-white dark:bg-neutral-800 shadow rounded-lg overflow-hidden">
-                <div id="omv-index-groups" class="border-b border-neutral-100 dark:border-neutral-700 overflow-x-auto">
-                    <livewire:insight.omv.index-groups />
-                </div>
-                <div class="flex w-full">
-                    <livewire:insight.omv.index-form />
-                </div>
-            </div>
-        </div>
-    </div> --}}
 </div>
