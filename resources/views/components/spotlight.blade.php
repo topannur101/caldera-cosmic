f96516b0b737f7a537d77ebc7bba5938cf64fcfe
andi.thea96@gmail.com <andi.thea96@gmail.com>
2024-10-06 22:50:18 +0700

    Fix bg

--- resources/views/components/spotlight.blade.php
+++ resources/views/components/spotlight.blade.php
@@ -60,7 +60,7 @@ $maxWidth = [
         x-transition:leave-start="opacity-100"
         x-transition:leave-end="opacity-0"
     >
-        <div class="absolute inset-0 bg-neutral-900 opacity-75"></div>
+        <div class="absolute inset-0 bg-neutral-900 opacity-95"></div>
     </div>
 
     <div
