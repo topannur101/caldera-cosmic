a31438bd865d03c6f85ba603b8f8f4ac9e746bd0
Andi Permana <andi.thea96@gmail.com>
2024-12-01 19:01:25 +0700

    Updates

--- resources/views/livewire/insight/stc/create-adj.blade.php
+++ resources/views/livewire/insight/stc/create-adj.blade.php
@@ -6,6 +6,8 @@ use App\Models\InsStcMachine;
 
 new class extends Component {
 
+    public bool $use_machine_sv = false;
+
     public function with(): array
     {
         return [
@@ -19,7 +21,7 @@ new class extends Component {
 <div>
     <h1 class="grow text-2xl text-neutral-900 dark:text-neutral-100 px-8">{{ __('Penyetelan') }}</h1>
     <div wire:key="modals">
-        <x-modal name="save-only">
+        <x-modal name="adj-save">
             <div class="p-6">
                 Simpan saja
                 Angka SV prediksi hanya akan di simpan sebagai catatan dan tidak akan di kirimkan ke HMI.
@@ -27,7 +29,7 @@ new class extends Component {
                 Input form: Keterangan
             </div>
         </x-modal>
-        <x-modal name="send">
+        <x-modal name="adj-send">
             <div class="p-6">
                 Kirim ke HMI
                 Angka SV prediksi akan di kirimkan ke HMI dan kamu harus menekan tombol Terapkan di HMI agar SV dapat berubah.
@@ -84,8 +86,7 @@ new class extends Component {
                         <div class="mb-1 text-xs uppercase font-normal leading-none text-neutral-400">6</div>
                         <div class="mb-1 text-xs uppercase font-normal leading-none text-neutral-400">7</div>
                         <div class="mb-1 text-xs uppercase font-normal leading-none text-neutral-400">8</div>
-
-                        <div>SV</div>    
+                        <div>PV</div>
                         <div>01</div>
                         <div>02</div>
                         <div>03</div>
@@ -95,7 +96,7 @@ new class extends Component {
                         <div>07</div>
                         <div>08</div>
 
-                        <div>PV</div>
+                        <div>SV</div>    
                         <div>01</div>
                         <div>02</div>
                         <div>03</div>
@@ -145,15 +146,29 @@ new class extends Component {
                     </div>               
                 </div>
                 <div>
-                    <label for="adj-formula_id"
-                    class="block px-3 mb-2 uppercase text-xs text-neutral-500">{{ __('SV Prediksi') }}</label>
-                    <x-select class="w-full" id="adj-formula_id" wire:model="formula_id">
-                        <option value=""></option>
-                        <option value="4">{{ __('Formula versi 4') }}</option>
-                    </x-select>
-                    @error('formula_id')
-                        <x-input-error messages="{{ $message }}" class="px-3 mt-2" />
-                    @enderror
+                    <div class="grid grid-cols-2 gap-x-3">
+                        <div>
+                            <label for="adj-formula_id"
+                            class="block px-3 mb-2 uppercase text-xs text-neutral-500">{{ __('Formula') }}</label>
+                            <x-select class="w-full" id="adj-formula_id" wire:model="formula_id">
+                                <option value=""></option>
+                                <option value="4">{{ __('Versi 4') }}</option>
+                            </x-select>
+                            @error('formula_id')
+                                <x-input-error messages="{{ $message }}" class="px-3 mt-2" />
+                            @enderror
+                        </div>
+                        <div>
+                            <label for="adj-use_machine_sv"
+                            class="block px-3 mb-2 uppercase text-xs text-neutral-500">{{ __('Pemilihan SV') }}</label>
+                            <div class="p-2">
+                                <x-toggle name="use_machine_sv" wire:model.live="use_machine_sv" :checked="$use_machine_sv ? true : false" >{{ __('Gunakan SV mesin') }}<x-text-button type="button"
+                                        class="ml-2" x-data="" x-on:click="$dispatch('open-modal', 'use_machine_sv-help')"><i
+                                            class="far fa-question-circle"></i></x-text-button>
+                                </x-toggle>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div>
                     <div class="grid grid-cols-9 text-center gap-x-3">
@@ -182,8 +197,8 @@ new class extends Component {
             <div class="flex gap-x-2 mt-6">
                 <div></div>
                 <div class="grow"></div>
-                <x-secondary-button type="button" x-on:click="$dispatch('open-modal', 'save-only')">{{ __('Simpan saja') }}</x-secondary-button>
-                <x-primary-button type="button" x-on:click="$dispatch('open-modal', 'send')">{{ __('Kirim') }}</x-primary-button>
+                <x-secondary-button type="button" x-on:click="$dispatch('open-modal', 'adj-save')">{{ __('Simpan saja') }}</x-secondary-button>
+                <x-primary-button type="button" x-on:click="$dispatch('open-modal', 'adj-send')">{{ __('Kirim') }}</x-primary-button>
             </div>
             <x-spinner-bg wire:loading.class.remove="hidden" wire:target.except="userq"></x-spinner-bg>
             <x-spinner wire:loading.class.remove="hidden" wire:target.except="userq" class="hidden"></x-spinner>
