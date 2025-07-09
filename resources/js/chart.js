331586d14980a8dd649111f97ecea48c7bdab21d
Andi <76712811+andimendunia@users.noreply.github.com>
2025-07-09 23:59:59 +0700

    Delete resources/js/chart.js

--- resources/js/chart.js
+++
@@ -1,8 +0,0 @@
-import Chart from 'chart.js/auto';
-import annotationPlugin from 'chartjs-plugin-annotation';
-import datalabelsPlugin from 'chartjs-plugin-datalabels';
-import 'chartjs-adapter-date-fns';
-Chart.register(annotationPlugin);
-Chart.register(datalabelsPlugin);
-window.Chart = Chart;
-
