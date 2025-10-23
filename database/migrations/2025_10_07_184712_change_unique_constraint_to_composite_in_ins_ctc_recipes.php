587efcc31c7522259df1194c92b8212668c23b18
Imam Pratama Setiady <imampratamasetiady11@gmail.com>
2025-10-23 19:25:38 +0700

    ubah mati

--- database/migrations/2025_10_07_184712_change_unique_constraint_to_composite_in_ins_ctc_recipes.php
+++ database/migrations/2025_10_07_184712_change_unique_constraint_to_composite_in_ins_ctc_recipes.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('ins_ctc_recipes', function (Blueprint $table) {
-            // Drop unique constraint pada name saja
-            $table->dropUnique('ins_ctc_recipes_name_unique');
+        // Schema::table('ins_ctc_recipes', function (Blueprint $table) {
+        //     // Drop unique constraint pada name saja
+        //     $table->dropUnique('ins_ctc_recipes_name_unique');
             
-            // Tambah composite unique constraint
-            // Kombinasi name + component_model + og_rs harus unique
-            $table->unique(['name', 'component_model', 'og_rs'], 'recipe_unique_combination');
-        });
+        //     // Tambah composite unique constraint
+        //     // Kombinasi name + component_model + og_rs harus unique
+        //     $table->unique(['name', 'component_model', 'og_rs'], 'recipe_unique_combination');
+        // });
     }
 
     /**
@@ -26,12 +26,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('ins_ctc_recipes', function (Blueprint $table) {
-            // Drop composite unique
-            $table->dropUnique('recipe_unique_combination');
+        // Schema::table('ins_ctc_recipes', function (Blueprint $table) {
+        //     // Drop composite unique
+        //     $table->dropUnique('recipe_unique_combination');
             
-            // Kembalikan unique pada name saja
-            $table->unique('name');
-        });
+        //     // Kembalikan unique pada name saja
+        //     $table->unique('name');
+        // });
     }
 };
