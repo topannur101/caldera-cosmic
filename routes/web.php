9e9038802ad8f8dc35fea883d4a8ca87768a4d87
topannur101 <topanmeuren@gmail.com>
2026-04-21 08:58:59 +0700

    fix: correct route name for OMV create index

--- routes/web.php
+++ routes/web.php
@@ -215,7 +215,7 @@ Route::prefix('insights')->group(function () {
         Volt::route('/omv/manage/models', 'insights.omv.manage.models')->name('manage.models');
         Volt::route('/omv/manage', 'insights.omv.manage.index')->name('manage.index');
         Volt::route('/omv/data', 'insights.omv.data.index')->name('data.index');
-        Volt::route('/omv/create', 'insights.omv.create.index-new')->name('create.index-new');
+        Volt::route('/omv/create', 'insights.omv.create.index')->name('create.index');
         Volt::route('/v1/omv/create', 'insights.omv.create.index-new')->name('create.index-new');
         Route::get('/omv', function () {
             if (auth()->check()) {
